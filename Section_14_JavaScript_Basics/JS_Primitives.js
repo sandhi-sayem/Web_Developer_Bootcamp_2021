3b30088d7ea1b89720a8739689f01db81859e46f
Sandhi Sayem <Sandhi.Sayem@Gmail.com>
2021-09-02 20:20:03 -0400

    Finished Section 14 JavaScript Basics

---
+++ Section_14_JavaScript_Basics/JS_Primitives.js
@@ -0,0 +1,11 @@
+console.log(2 + 2);
+
+let myLuckyNumber = 37;
+let octopusLimbs = 8;
+
+const boilingPointC = 100;
+const boilingPointF = 212;
+
+let isGameOver = true;
+
+console.log(isGameOver);
